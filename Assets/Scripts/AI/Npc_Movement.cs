fa23e924e1cab8459a5ee635558ee7bcbf1469d1
Gabriel Cassimiro <gdinizcassimiro@gmail.com>
2019-04-30 19:38:52 -0300

    Gerador de NavMesh Bake em tempo real ao mapa para utilização de IA dentro do mapa criado

---
+++ Assets/Scripts/AI/Npc_Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Npc_Movement : MonoBehaviour
+{
+    [SerializeField] Transform _SetDestiny;
+
+    private NavMeshAgent agent;
+
+    private void Start() {
+        agent = GetComponent<NavMeshAgent>();
+
+        if(agent == null)
+            Debug.Log("Objeto Sem NavMeshAgent!");
+        else
+            SetDestination();
+    }
+
+    private void SetDestination(){
+        Vector3 targetVector = _SetDestiny.transform.position;
+        agent.SetDestination(targetVector);
+    }
+}
---
+++ Assets/Scripts/NavMesh/NavMeshBaker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshBaker : MonoBehaviour
+{
+    [SerializeField] private NavMeshSurface[] navMeshSurfaces;
+    [SerializeField] private GameObject mapRoot;
+
+    private void Start() {
+        getComponents();
+    }
+
+    private void getComponents(){
+        mapRoot = GameObject.Find("MapRoot");
+        getNavMesh();
+    }
+
+    private void getNavMesh(){
+        navMeshSurfaces = mapRoot.GetComponentsInChildren<NavMeshSurface>();
+        if(navMeshSurfaces != null){
+            CreateBake();
+        }
+    }
+
+    private void CreateBake(){
+        for(int i = 0 ; i < navMeshSurfaces.Length; i++){
+            navMeshSurfaces[i].BuildNavMesh();
+        }
+    }
+}
