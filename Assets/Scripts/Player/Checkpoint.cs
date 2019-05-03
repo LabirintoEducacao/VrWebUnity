b7ba81e616749e2e85ad7407712e2d87425d6515
Gabriel Cassimiro <gdinizcassimiro@gmail.com>
2019-05-03 13:34:55 -0300

    Bug perdendo o ultimo button

--- Assets/Scripts/Player/Checkpoint.cs
+++ Assets/Scripts/Player/Checkpoint.cs
@@ -4,24 +4,67 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    [Header("Components")]
+    [SerializeField] private BoxCollider collider;
+
+    [Header("Tags, Names and Layers")]
+    [SerializeField] private string _tagButton = "Button";
+    [SerializeField] private string _tagPlayer = "Player";
+
     [Header("Buttons")]
     [SerializeField] private GameObject[] _Buttons;
     [SerializeField] private GameObject[] _Temp;
-    [SerializeField] private string _tagButton = "Button";
 
     [Header("Variables")]
     [SerializeField] private float _RangeLimit;
+    [SerializeField] private int size = 0;
 
     private void Start() {
+        collider = GetComponent<BoxCollider>();
+
         _Temp = GameObject.FindGameObjectsWithTag(_tagButton);
-        _Buttons = GameObject.FindGameObjectsWithTag(_tagButton);
+        // _Buttons = GameObject.FindGameObjectsWithTag(_tagButton);
         
         getButtons();
+
+        for (int i = 0; i < _Buttons.Length; i++)
+        {
+            _Buttons[i].SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag(_tagPlayer)){
+            for (int i = 0; i < _Buttons.Length; i++)
+            {
+                _Buttons[i].SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.CompareTag(_tagPlayer)){
+            for (int i = 0; i < _Buttons.Length; i++)
+            {
+                _Buttons[i].SetActive(false);
+            }
+        }
     }
 
     private void getButtons(){
         int i = 0;
         int j = 0;
+
+        
+
+        foreach (GameObject item in _Temp)
+        {
+            if((item.transform.position - transform.position).magnitude < _RangeLimit){
+                size++;
+            }
+        }
+        _Buttons = new GameObject[size];
+
         foreach (GameObject item in _Temp)
         {
             if((item.transform.position - transform.position).magnitude < _RangeLimit){
@@ -32,13 +75,40 @@ public class Checkpoint : MonoBehaviour
             j++;
         }
 
-        int Length = _Buttons.Length - 1;
-        for (int k = 0; k < _Buttons.Length; k++)
+        _Buttons = new GameObject[size];
+
+        int Length = _Temp.Length - 1;
+
+        foreach (GameObject item in _Buttons)
         {
-            if(_Buttons[Length] == _Buttons[k] || _Buttons[k] == null){
-                _Buttons[Length] = null;
+            if(item == null){
+                size--;
             }
         }
+
+        //_Buttons = new GameObject[size];
+        
+        
+        
+
+
+        // foreach (GameObject item in _Temp)
+        // {
+        //     if((item.transform.position - transform.position).magnitude < _RangeLimit){
+        //         _Buttons[i] = item;
+        //         i++;
+        //         _Buttons[j] = null;
+        //     }
+        //     j++;
+        // }
+
+        // int Length = _Buttons.Length - 1;
+        // for (int k = 0; k < _Buttons.Length; k++)
+        // {
+        //     if(_Buttons[Length] == _Buttons[k] || _Buttons[k] == null){
+        //         _Buttons[Length] = null;
+        //     }
+        // }
     }
 
     private void OnDrawGizmos() {
