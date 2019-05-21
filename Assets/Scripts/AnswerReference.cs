0cc20dfca0e1ae1806fa8b17b25e4e89286385c6
Gabriel Cassimiro <gdinizcassimiro@gmail.com>
2019-05-21 14:54:14 -0300

    RoomManager instancia os objects de resposta na sala

---
+++ Assets/Scripts/AnswerReference.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AnswerReference : MonoBehaviour
+{
+    public TextMeshPro textPanel;
+    public GameObject panel;
+
+    private void Start()
+    {
+       //panel.SetActive(false);
+    }
+
+    //public void ActivePanel()
+    //{
+    //    panel.SetActive(true);
+    //}
+    //public void DesactivePanel()
+    //{
+    //    panel.SetActive(false);
+    //}
+}
--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class RoomManager : MonoBehaviour
 {
+    
+    public Transform[] spawnAnswer;
+    public AnswerReference[] answerReference;
+    public GameObject answerPrefab;
+
+    [Header("room data")]
     public int starting_question_id;
     public float time_limit;
     public int theme;
     public Question question;
+    public Answer[] answers;
+
+    private void Start()
+    {
+        GameObject[] answerTemp = new GameObject[spawnAnswer.Length];
+        answerReference = new AnswerReference[spawnAnswer.Length];
+        answers = question.answers;
+        for (int i = 0; i < spawnAnswer.Length; i++)
+        {
+            answerTemp[i] = Instantiate(answerPrefab, spawnAnswer[i].position, spawnAnswer[i].rotation);
+        }
+
+        for (int i = 0; i < answerTemp.Length; i++)
+        {
+            answerReference[i] = answerTemp[i].GetComponent<AnswerReference>();
+            answerReference[i].textPanel.text = answers[i].answer;
+        }
+    }
 }
