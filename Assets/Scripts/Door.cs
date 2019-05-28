011c26c45b198f472b9ac8ef23f51ec8500fdd44
Gabriel Cassimiro <gdinizcassimiro@gmail.com>
2019-05-28 12:08:27 -0300

    Interação da resposta com a porta em desenvolvimento

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    /* Quando acontecer uma interação com a porta, o script deve verificar se a reposta(item)
+     * que o jogador carrega com ele é o que deve ser utilizado para abrir a porta em que este
+     * script se encontra.
+     * 
+     * Caso seja a resposta errada, o jogador perderá uma estrela.
+     */
+}
