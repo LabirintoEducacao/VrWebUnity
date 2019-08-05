75e9af5c5bc352712e6af61a0adf86e566149bc1
mrigon <marcelo.rigon@gmail.com>
2019-08-05 14:03:43 -0300

    algumas adições de eventos pro Firebase.

--- Assets/Scripts/Support/FirebaseInitializer.cs
+++ Assets/Scripts/Support/FirebaseInitializer.cs
@@ -1,39 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Firebase.Analytics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class FirebaseInitializer : MonoBehaviour {
-    public static bool firebaseLoaded = false;
-    public static Firebase.FirebaseApp app;
+    static FirebaseInitializer _instance;
+    public static FirebaseInitializer instance {
+        get {
+            if (_instance == null) {
+                GameObject go = new GameObject ();
+                _instance = go.AddComponent<FirebaseInitializer> ();
+            }
+            return _instance;
+        }
+    }
+    public static bool firebaseLoaded { get { return app != null; } }
+    public static Firebase.FirebaseApp app = null;
+    bool initializing = false;
 
     private void Awake () {
-        Firebase.FirebaseApp.CheckAndFixDependenciesAsync ().ContinueWith (task => {
-            var dependencyStatus = task.Result;
-            if (dependencyStatus == Firebase.DependencyStatus.Available) {
-                // Create and hold a reference to your FirebaseApp,
-                // where app is a Firebase.FirebaseApp property of your application class.
-                app = Firebase.FirebaseApp.DefaultInstance;
-
-                firebaseLoaded = true;
-
-                // FirebaseAnalytics.SetCurrentScreen (SceneManager.GetActiveScene ().name);
-                // googleAnalytics.LogScreen ("Main Menu");
-            } else {
-                UnityEngine.Debug.LogError (System.String.Format (
-                    "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
-                // Firebase Unity SDK is not safe to use here.
-            }
-        });
-    }
-    // Start is called before the first frame update
-    void Start () {
-
+        if ((_instance != null) && (FirebaseInitializer._instance != this)) {
+            Destroy (this.gameObject);
+        } else {
+            _instance = this;
+            DontDestroyOnLoad (this.gameObject);
+            initialize ();
+        }
     }
 
-    // Update is called once per frame
-    void Update () {
+    private void initialize () {
+        if (!initializing && !firebaseLoaded) {
+            Debug.Log ("Firebase Initialization starting.");
+            this.initializing = true;
+            Firebase.FirebaseApp.CheckAndFixDependenciesAsync ().ContinueWith (task => {
+                var dependencyStatus = task.Result;
+                Debug.Log ("Firebase Initialization completed with status: " + dependencyStatus);
+                this.initializing = false;
+                if (dependencyStatus == Firebase.DependencyStatus.Available) {
+                    // Create and hold a reference to your FirebaseApp,
+                    // where app is a Firebase.FirebaseApp property of your application class.
+                    app = Firebase.FirebaseApp.DefaultInstance;
 
+                    FirebaseAnalytics
+                        .LogEvent (FirebaseAnalytics.EventAppOpen);
+                    SceneManager.activeSceneChanged += OnSceneChanged;
+
+                } else {
+                    UnityEngine.Debug.LogError (System.String.Format (
+                        "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                    // Firebase Unity SDK is not safe to use here.
+                }
+            });
+        }
+    }
+
+    private void OnSceneChanged(Scene oldScene, Scene newScene)
+    {
+        FirebaseAnalytics
+            .LogEvent( "navigation", FirebaseAnalytics.ParameterLevelName, newScene.name);
     }
 }
