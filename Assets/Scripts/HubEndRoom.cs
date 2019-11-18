21e8d1527fff2547cc96901c8ead548f957a51a4
Felipe Inoue <felipeinoue@yahoo.com.br>
2019-11-18 13:47:07 -0300

    Colocado pontuação por porcentagem

--- Assets/Scripts/HubEndRoom.cs
+++ Assets/Scripts/HubEndRoom.cs
@@ -2,42 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HubEndRoom : MonoBehaviour
-{
+public class HubEndRoom : MonoBehaviour {
 
-    [Header("UI")]
-    public TMPro.TextMeshProUGUI FinalText;
+	[Header("UI")]
+	public TMPro.TextMeshProUGUI FinalText;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        //TODO Criar Panel com Score
-        // Adiciona Texto
-        SaveGameData svgd = DataManager.manager.savegame;
-        var sb = new System.Text.StringBuilder();
+	private void OnTriggerEnter(Collider other) {
+		//TODO Criar Panel com Score
+		// Adiciona Texto
+		SaveGameData svgd = DataManager.manager.savegame;
+		var sb = new System.Text.StringBuilder();
 
-        
-        // Formata o tempo
-        double sec = System.Math.Truncate(svgd.timeElapsed);
+		// Formata o tempo
+		double sec = System.Math.Truncate(svgd.timeElapsed);
 
-        int min = 0;
-        int hour = 0;
-        while(sec >= 60f)
-        {
-            sec -= 60f;
-            min++;
+		int min = 0;
+		int hour = 0;
+		while (sec >= 60f) {
+			sec -= 60f;
+			min++;
 
-            if (min == 60)
-            {
-                min = 0;
-                hour++;
-            }
-        }
+			if (min == 60) {
+				min = 0;
+				hour++;
+			}
+		}
 
-        sb.AppendLine(System.String.Format("Tempo Total: {0}h {1}m {2}s", hour, min, sec));
-        sb.AppendLine(System.String.Format("Pontuação: {0}", svgd.score));
-        sb.AppendLine(System.String.Format("Acertos: {0}", svgd.rightAnswers));
-        sb.AppendLine(System.String.Format("Erros: {0}", svgd.wrongAnswers));
+		int total = svgd.rightAnswers + svgd.wrongAnswers;
+		double perScore = 0f;
+		if (total != 0) {
 
-        FinalText.text = sb.ToString();
-    }
+			perScore = (svgd.rightAnswers / total) * 100;
+			perScore = System.Math.Truncate(perScore);
+		}
+
+		sb.AppendLine(System.String.Format("Tempo Total: {0}h {1}m {2}s", hour, min, sec));
+		sb.AppendLine(System.String.Format("Pontuação: {0}%", perScore));
+		sb.AppendLine(System.String.Format("Acertos: {0}", svgd.rightAnswers));
+		sb.AppendLine(System.String.Format("Erros: {0}", svgd.wrongAnswers));
+
+		FinalText.text = sb.ToString();
+	}
 }
