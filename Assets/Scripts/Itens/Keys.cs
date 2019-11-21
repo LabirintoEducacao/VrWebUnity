e2a5d32c9a5a505e1ec35651da3e737c30d4d9e0
Felipe Inoue <felipeinoue@yahoo.com.br>
2019-11-21 12:55:54 -0300

    Animação na chave

--- Assets/Scripts/Itens/Keys.cs
+++ Assets/Scripts/Itens/Keys.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class Keys : ItemBase {
 
+	private void Start() {
+		GetComponent<Animator>().SetBool("show", false);
+	}
+
 	public override void ActionItem() {
 		currentRoom.PositionNextRoom("DoorAnswer", properties.correct);
 	}
+
+	void OnEnable() {
+		GetComponent<Animator>().SetBool("show", true);
+	}
+
+
 }
