e65c23299060f4a65398d7e9563d084d027094a3
Marcelo F. Rigon <marcelo.rigon@gmail.com>
2019-12-06 11:28:39 -0300

    tela de listagem das salas montadas com prefab para a listagem. Falta fazer funcionar e adicionar no animator.

--- Assets/Scripts/Menu/RoomGridDisplay.cs
+++ Assets/Scripts/Menu/RoomGridDisplay.cs
@@ -6,17 +6,37 @@ using UnityEngine.UI;
 
 public class RoomGridDisplay : MonoBehaviour
 {
-	public TextMeshProUGUI textHolder;
+	public TextMeshProUGUI roomNameText;
+	public TextMeshProUGUI roomIDText;
+	public TextMeshProUGUI roomQuestionsText;
+	public TextMeshProUGUI roomStatusText;
+	public bool oneText = true;
+
+	public Sprite lockedImage;
+	public Sprite unlockedImage;
+	public Image permissionImage;
+
 	public WebRoomInfo roomInfo { get; set; }
 	public bool isLoaded = false;
 
 
 	private void LateUpdate() {
 		if (roomInfo != null) {
-			textHolder.text = roomLabel();
+			if (this.oneText) {
+				roomNameText.text = roomLabel();
+			} else {
+				this.roomNameText.text = this.roomInfo.name;
+				this.roomIDText.text = this.roomInfo.id.ToString();
+				this.roomQuestionsText.text = string.Format("{0}/{1}", new object[] {
+						roomInfo.Pergunta-roomInfo.Reforco,
+						roomInfo.Pergunta 
+				});
+				this.roomStatusText.text = roomInfo.progressName;
+				this.permissionImage.sprite = roomInfo.isPublic ? this.unlockedImage : this.lockedImage;
+			}
 			this.GetComponent<Button>().interactable = true;
 		} else {
-			textHolder.text = "";
+			roomNameText.text = "";
 			this.GetComponent<Button>().interactable = false;
 		}
 	}
