773d4ddc1c1aee58b7b4ae7dd955ef7d2c397a5e
fiwon123 <felipeinoue@yahoo.com.br>
2020-04-07 15:18:16 -0300

    deploy android

--- Assets/Scripts/Menu/BackToPreviousMenu.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BackToPreviousMenu : MonoBehaviour {
-	public void OnClick() {
-		this.GetComponentInParent<Animator>().SetTrigger("Back");
-	}
-}
--- Assets/UnityBarCode/Samples/README.md
+++
@@ -1,5 +0,0 @@
-# Unity Barcode Scanner
-
-For the moment, two samples are available :
-* Simple : Just display the camera with possibility to Start / Stop the scan
-* Continous : Always running scan with possiblity to scan multiple barcode
