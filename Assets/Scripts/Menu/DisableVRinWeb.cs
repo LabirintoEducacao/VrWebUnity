49289ab2cef83a8306b63d2a18c0eafbc297dcc3
Carol Salvato <carol_lgl@hotmail.com>
2020-05-04 19:08:57 -0300

    #96-tirar-vr-mode-da-tela-inicial-na-versao-web

---
+++ Assets/Scripts/Menu/DisableVRinWeb.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableVRinWeb : MonoBehaviour
+{
+    public GameObject vrButton;
+    void Start()
+    {
+        #if UNITY_WEBGL
+            vrButton.SetActive(false);
+        #endif
+    }
+}
