12a0814157e98d20053136dfa76dd654028123ba
Carol Salvato <carol_lgl@hotmail.com>
2020-05-13 18:18:20 -0300

    #90-desenvolver-controlador-de-sons

---
+++ Assets/Scripts/Audio/AudioList.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*TODO: Artes para menu de configuração e musicas
+* TODO: Colocar o HUD do menu de config em todas as cenas em que precisa ser acessado
+* TODO: Manter o volume após mudar de cena
+* TODO: Colocar objetos de Audio (3) em todas as cenas
+* TODO: Para cada audio, criar e vincular objeto de audio, e adicionar função (Para BGM apenas função de play, para SFX função de play e stop)
+*/
+public class AudioList : MonoBehaviour
+{
+    //BGM
+    [SerializeField] private AudioClip menuBGM;
+    [SerializeField] private AudioClip gameGeloBGM;
+    [SerializeField] private AudioClip gameFlorestaBGM;
+    [SerializeField] private AudioClip gameCasaBGM;
+    [SerializeField] private AudioClip gameUrbanoBGM;
+    [SerializeField] private AudioClip gameCavernaBGM;
+    [SerializeField] private AudioClip gameDesertoBGM;
+    //SFX
+    [SerializeField] private AudioClip buttonClick;
+    [SerializeField] private AudioClip playerPassos;
+    [SerializeField] private AudioClip playerAlavanca;
+    [SerializeField] private AudioClip playerChave;
+    
+    //Funções BGM
+    public void PararMusica(){ AudioMixerSource.instance.PararBGM(); }
+    public void PlayMenuBGM(){ AudioMixerSource.instance.TocarBGM(menuBGM); }
+    public void PlayGameGeloBGM(){ AudioMixerSource.instance.TocarBGM(gameGeloBGM); }
+    
+    //Funções SFX
+    public void PararEfeitos(){ AudioMixerSource.instance.PararSFX(); }
+    public void PlayButtonClick(){ AudioMixerSource.instance.TocarSFX(buttonClick); }
+    public void PlayPlayerPassos(){ AudioMixerSource.instance.TocarSFX(playerPassos); }
+    public void PlayPlayerAlavanca(){ AudioMixerSource.instance.TocarSFX(playerAlavanca); }
+    public void PlayPlayerChave(){ AudioMixerSource.instance.TocarSFX(playerChave); }
+    
+}
---
+++ Assets/Scripts/Audio/VolumeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeController : MonoBehaviour
+{
+    [SerializeField] public AudioMixer masterMixer;
+    [SerializeField] public Slider bgmSlider;
+    [SerializeField] public Slider sfxSlider;
+
+    private void Start()
+    {
+        VolumeBGM();
+        VolumeSFX();
+    }
+
+    public void VolumeBGM()
+    {
+        masterMixer.SetFloat("BGM", bgmSlider.value);
+    }
+
+    public void VolumeSFX()
+    {
+        masterMixer.SetFloat("SFX", sfxSlider.value);
+    }
+}
