bd21030f914b01846fa081e46a9f2d5d9c1caccb
Carol Salvato <carol_lgl@hotmail.com>
2020-05-14 18:58:43 -0300

    #90-desenvolver-controlador-de-sons

--- Assets/Scripts/Audio/VolumeController.cs
+++ Assets/Scripts/Audio/VolumeController.cs
@@ -9,9 +9,20 @@ public class VolumeController : MonoBehaviour
     [SerializeField] public AudioMixer masterMixer;
     [SerializeField] public Slider bgmSlider;
     [SerializeField] public Slider sfxSlider;
+    [SerializeField] private static float sfxValueVolume;
+    [SerializeField] private static float bgmValueVolume = -30;
+
+    public static VolumeController instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
 
     private void Start()
     {
+        bgmSlider.value = bgmValueVolume;
+        sfxSlider.value = sfxValueVolume;
         VolumeBGM();
         VolumeSFX();
     }
@@ -19,10 +30,12 @@ public class VolumeController : MonoBehaviour
     public void VolumeBGM()
     {
         masterMixer.SetFloat("BGM", bgmSlider.value);
+        bgmValueVolume = bgmSlider.value;
     }
 
     public void VolumeSFX()
     {
         masterMixer.SetFloat("SFX", sfxSlider.value);
+        sfxValueVolume = sfxSlider.value;
     }
 }
